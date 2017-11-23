1565609582e660f00db2814e8690f2c6541264d2
Michael Siepmann <misi1967@yahoo.de>
2017-11-24 00:30:01 +0100

    Unit-Test improved

--- src/test/java/org/codinjutsu/tools/nosql/elasticsearch/logic/commands/GetIndicesTest.kt
+++ src/test/java/org/codinjutsu/tools/nosql/elasticsearch/logic/commands/GetIndicesTest.kt
@@ -2,22 +2,26 @@ package org.codinjutsu.tools.nosql.elasticsearch.logic.commands
 
 import org.apache.commons.httpclient.HttpClient
 import org.apache.commons.httpclient.HttpClientStub
+import org.apache.commons.httpclient.methods.GetMethod
 import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class GetIndicesTest {
 
-    private val httpClient: HttpClient = HttpClientStub(200, RESPONSE)
+    private val httpClient: HttpClientStub = HttpClientStub(200, RESPONSE)
 
     @Test
     fun execute() {
         val result = GetIndicesStub(httpClient).execute()
+        assertTrue(httpClient.calledMethod is GetMethod)
+        assertEquals("/dummyindex/_aliases", httpClient.calledMethod?.uri.toString())
         assertEquals("test", result.get("index").asString)
         assertEquals("testtype", result.get("type").asString)
     }
 
-    private class GetIndicesStub(private val httpClient: HttpClient) : GetIndices("") {
+    private class GetIndicesStub(private val httpClient: HttpClient) : GetIndices("/dummyindex") {
 
         override fun createClient() = httpClient
     }
